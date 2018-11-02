169962a2f47c8b21d2ce9a5a3afcc52cc2e81db5
ksgl <ksenia.glazacheva@gmail.com>
2018-11-02 05:19:25 +0300

    password now stored as a byte array

--- utiles/database/schema.sql
+++ utiles/database/schema.sql
@@ -2,14 +2,14 @@
 CREATE TABLE userinfo (
     uid         BIGSERIAL       PRIMARY KEY,
     username    TEXT            NOT NULL,
-    password    TEXT            NOT NULL,
+    password    BYTEA           NOT NULL,
     score       INT             DEFAULT 0, 
-    avatar      TEXT            DEFAULT '/some/paths/lead/to/beautiful/destinations/'
+    avatar      BYTEA           DEFAULT '/some/path'
 );
 
 CREATE TABLE session (
     uid         SERIAL,
-    cookie   TEXT            DEFAULT '',
+    cookie      TEXT            DEFAULT '',
 
     FOREIGN KEY (uid) REFERENCES userinfo(uid)
 );
