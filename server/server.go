38060adc69ee05643f88f05020e7c61076c5b0ce
ksgl <ksenia.glazacheva@gmail.com>
2018-11-07 11:27:23 +0300

    ws working process: middlewares rearranged

--- server/server.go
+++ server/server.go
@@ -27,16 +27,16 @@ func Start(path string) {
 	r.HandleFunc("/", mw.Chain(API.SlashHandler, mw.Auth(), mw.CORS(conf.CC))).Methods("GET")
 	r.HandleFunc("/users", mw.Chain(API.RegisterPOSTHandler, mw.CORS(conf.CC))).Methods("POST")
 	r.HandleFunc("/users/me", mw.Chain(API.MeGETHandler, mw.Auth(), mw.CORS(conf.CC))).Methods("GET")
-	r.HandleFunc("/users/me", mw.Chain(API.EditMePUTHandler, mw.CORS(conf.CC), mw.Auth())).Methods("PUT")
+	r.HandleFunc("/users/me", mw.Chain(API.EditMePUTHandler, mw.Auth(), mw.CORS(conf.CC))).Methods("PUT")
 	r.HandleFunc("/users/{name}", mw.Chain(API.UserGETHandler, mw.CORS(conf.CC))).Methods("GET")
 	r.HandleFunc("/users/leaders", mw.Chain(API.LeadersGETHandler, mw.CORS(conf.CC))).Methods("GET")
 	r.HandleFunc("/session", mw.Chain(API.LoginPOSTHandler, mw.CORS(conf.CC))).Methods("POST")
-	r.HandleFunc("/session", mw.Chain(API.LogoutDELETEHandler, mw.CORS(conf.CC), mw.Auth())).Methods("DELETE")
+	r.HandleFunc("/session", mw.Chain(API.LogoutDELETEHandler, mw.Auth(), mw.CORS(conf.CC))).Methods("DELETE")
 
 	r.HandleFunc("/users/me", mw.Chain(API.EditMeOPTHandler, mw.OptionsPreflight(conf.CC))).Methods("OPTIONS")
 	r.HandleFunc("/session",  mw.Chain(API.LogoutOPTHandler, mw.OptionsPreflight(conf.CC))).Methods("OPTIONS")
 
-	r.HandleFunc("/conn/lobby", mw.Chain(API.LobbyHandler, mw.CORS(conf.CC), mw.WebSocketHeadersCheck())).Methods("GET")
+	r.HandleFunc("/conn/lobby", mw.Chain(API.LobbyHandler, mw.WebSocketHeadersCheck(), mw.CORS(conf.CC))).Methods("GET")
 
 	http.ListenAndServe(conf.SC.Port, handlers.RecoveryHandler()(r))
 }
