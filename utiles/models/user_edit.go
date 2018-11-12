314ae77a6a25767f683a1098b39715de40510f96
ksgl <ksenia.glazacheva@gmail.com>
2018-11-13 01:50:44 +0300

    omitempty

--- utiles/models/user_edit.go
+++ utiles/models/user_edit.go
@@ -2,7 +2,7 @@ package models
 
 // easyjson:json
 type UserEdit struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
-	Avatar   string `json:"avatar"`
+	Username string `json:"username",omitempty`
+	Password string `json:"password",omitempty`
+	Avatar   string `json:"avatar",omitempty`
 }
