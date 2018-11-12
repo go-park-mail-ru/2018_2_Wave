e0acecb8fa566e960af5701efbabffb08331090f
ksgl <ksenia.glazacheva@gmail.com>
2018-11-13 01:48:47 +0300

    dbconf gone

--- utiles/config/config.go
+++ utiles/config/config.go
@@ -1,27 +1,19 @@
 package config
 
 import (
-	"fmt"
 	"io/ioutil"
 )
 
 // easyjson:json
 type Configuration struct {
-	SC ServerConfiguration   `json:"server"`
-	DC DatabaseConfiguration `json:"database"`
-	CC CORSConfiguration     `json:"cors"`
+	SC ServerConfiguration `json:"server"`
+	CC CORSConfiguration   `json:"cors"`
 }
 
 // easyjson:json
 type ServerConfiguration struct {
 	Host string `json:"host"`
 	Port string `json:"port"`
-}
-
-// easyjson:json
-type DatabaseConfiguration struct {
-	User   string `json:"user"`
-	DBName string `json:"dbname"`
 }
 
 // easyjson:json
@@ -37,12 +29,13 @@ func Configure(path string) Configuration {
 	data, err := ioutil.ReadFile(path)
 
 	if err != nil {
-		data = []byte("{}")
+		//data = []byte("{}")
 		return config
 	}
 
 	if err := config.UnmarshalJSON(data); err != nil {
-		fmt.Printf("Incorrect config json: %s\nError: %v", data, err)
+		//fmt.Printf("Incorrect config json: %s\nError: %v", data, err)
+		return config
 	}
 
 	return config
