492a0280a442b734c245fb5d1e6ef3c859f77ef1
ksgl <ksenia.glazacheva@gmail.com>
2018-11-25 12:05:39 +0300

    cleanup

--- server/chat/chat/chat.go
+++ server/chat/chat/chat.go
@@ -1,7 +1,7 @@
 package chat
 
 import (
-	"Wave/server/room"
+	"Wave/server/chat/room"
 	"time"
 )
 
@@ -9,16 +9,10 @@ type App struct {
 	*room.Room
 }
 
-// New snake app
 func New(id room.RoomID, step time.Duration) room.IRoom {
 	s := &App{
 		Room: room.NewRoom(id, step),
-		world: newWorld(sceneSize{
-			X: 900,
-			Y: 900,
-		}),
 	}
-	s.OnTick = s.onTick
-	s.Routes["game_info"] = s.onGameInfo
+
 	return s
 }
