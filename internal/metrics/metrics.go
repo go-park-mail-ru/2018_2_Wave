39bfa233dec0e067c196941d6f18134cc1a86bc9
ksgl <ksenia.glazacheva@gmail.com>
2018-11-28 17:47:13 +0300

    fixed metrics

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -27,28 +27,10 @@ func Construct() *Profiler {
 			Name: "active_rooms",
 			Help: "Number of active rooms.",
 		}),
-
-		CPUUsage: prometheus.NewGauge(prometheus.GaugeOpts{
-			Name: "cpu_usage",
-			Help: "Info on CPU usage.",
-		}),
-
-		MemUsage: prometheus.NewGauge(prometheus.GaugeOpts{
-			Name: "mem_usage",
-			Help: "Info on mem usage.",
-		}),
-
-		DiskUsage: prometheus.NewGauge(prometheus.GaugeOpts{
-			Name: "disk_usage",
-			Help: "Info on disk usage.",
-		}),
 	}
 
 	prometheus.MustRegister(p.HitsStats)
 	prometheus.MustRegister(p.ActiveRooms)
-	prometheus.MustRegister(p.CPUUsage)
-	prometheus.MustRegister(p.MemUsage)
-	prometheus.MustRegister(p.DiskUsage)
 
 	return &p
 }
