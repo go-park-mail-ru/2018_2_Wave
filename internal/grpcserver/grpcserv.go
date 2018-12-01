9a5785f075a1b4ee62daa96c49020e9e49c59800
ksgl <ksenia.glazacheva@gmail.com>
2018-12-01 23:01:30 +0300

    now logger takes args

--- internal/grpcserver/grpcserv.go
+++ internal/grpcserver/grpcserv.go
@@ -15,26 +15,6 @@ import (
 	"google.golang.org/grpc"
 )
 
-func StartAuthServer(curlog *lg.Logger, GRPCC config.AuthConfiguration, db *database.DatabaseModel) {
-	lis, err := net.Listen("tcp", GRPCC.Port)
-	if err != nil {
-
-		curlog.Sugar.Infow("can't listen on port",
-		"source", "grpcserv.go",
-		"who", "New")
-
-	}
-
-	server := grpc.NewServer()
-	auth.RegisterAuthServer(server, au.NewAuthManager(curlog, db))
-
-	curlog.Sugar.Infow("starting grpc server on " + GRPCC.Host + GRPCC.Port,
-		"source", "grpcserv.go",
-		"who", "New")
-
-	go server.Serve(lis)
-}
-
 func startGameServer(curlog *lg.Logger, conf config.Configuration, Prof *mc.Profiler) {
 	lis, err := net.Listen("tcp", conf.Game.Port)
 	if err != nil {
